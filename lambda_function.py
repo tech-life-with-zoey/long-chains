e631adf2bd4761522c2db56492967a9555142923
Daniel Furman <djfurman@gmail.com>
2018-11-04 16:52:11 -0500

    Add short chain calculations.

---
+++ lambda_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def compute_short_chain(number):
+    """Compute a short chain
+
+    Arguments:
+        number {int} -- Requested number for which to compute the square
+
+    Returns:
+        int -- Value of the square/short chain for the given number
+    """
+
+    return number * number
+
+def lambda_handler(event, context):
+    pass
